4c71dde61ed37b6c569b41e2f1ae9a0eccc969b8
Luve <luvemashru@gmail.com>
2023-08-14 17:45:49 +0530

    Resolved CORS error | Needed post UI Changes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sanic import Sanic
 from pymongo import MongoClient
+from sanic_cors import CORS
 from url import app_routes
 from database import TrackerDB
 
 app = Sanic(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Import routes from url.py
 app.blueprint(app_routes)
